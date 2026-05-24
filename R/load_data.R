1ea9a2b6c9f3fe2bab8bb0b20f3fdb796c58c819
Noah Nayve <npnayve@gmail.com>
2026-05-23 22:23:33 -0700

    edited

--- R/load_data.R
+++ R/load_data.R
@@ -3,9 +3,9 @@
 #' Loads the plastic waste dataset directly
 #' from TidyTuesday
 #'
-#' @return A data frame with columns including country, year, parent_company,
+#' @return A data frame with columns that include country, year, parent_company,
 #'   plastic type totals (hdpe, ldpe, pet, pp, ps, pvc), grand_total,
-#'   num_events, and volunteers.
+#'   num_events, volunteer, and other country level metrics.
 #' @export
 #' @importFrom readr read_csv
 #' @examples
